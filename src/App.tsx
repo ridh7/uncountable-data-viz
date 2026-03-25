0853bd5219012b33d14cac469d5a01cd19241ef3
Ridhwik Kalgaonkar <rk44@illinois.edu>
2026-03-25 16:57:31 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-[#2329AF]">
+        Uncountable Data Viz
+      </h1>
+    </div>
+  );
+}
+
+export default App;
