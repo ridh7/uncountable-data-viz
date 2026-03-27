a3b1899e70d5e4666bb06aacde4a8ad604b6e97c
Ridhwik Kalgaonkar <rk44@illinois.edu>
2026-03-27 10:09:54 -0500

    Replace inline SVGs with components

--- src/components/scatterplot/ScatterPlot.tsx
+++ src/components/scatterplot/ScatterPlot.tsx
@@ -1,5 +1,6 @@
 import { useState, useMemo, useRef, useCallback } from "react";
 import Button from "../Button.tsx";
+import { DownloadIcon } from "../icons.tsx";
 import {
   ScatterChart,
   Scatter,
@@ -210,21 +211,7 @@ function ScatterPlot({ experiments }: ScatterPlotProps) {
             </div>
           </div>
           <Button onClick={exportPng}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="14"
-              height="14"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2.5"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-              <polyline points="7 10 12 15 17 10" />
-              <line x1="12" y1="15" x2="12" y2="3" />
-            </svg>
+            <DownloadIcon />
           </Button>
         </div>
       </div>
